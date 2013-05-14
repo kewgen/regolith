96f9cba87bbec2111b1ae54970c74f5d53aecee5
abarakov <abarakov.geargames@gmail.com>
2013-05-14 15:16:48 +0400

    Исправил недочет в BattleMapSerializer

--- server/src/com/geargames/regolith/serializers/BattleMapSerializer.java
+++ server/src/com/geargames/regolith/serializers/BattleMapSerializer.java
@@ -83,6 +83,7 @@ public class BattleMapSerializer {
             }
         }
         SimpleSerializer.serialize(size, buffer);
+        size = cells[x][y].getSize();
         for (int i = 0; i < size; i++) {
             CellElement element = elements[i];
             switch (element.getElementType()) {
